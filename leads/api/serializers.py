0b3a85e57c696d48e84016623f8e61f58029639c
chargik <unklerufus@gmail.com>
2017-12-27 19:27:02 +0300

    Added ajax form validation

--- leads/api/serializers.py
+++ leads/api/serializers.py
@@ -9,19 +9,15 @@ class JoinSerializer(serializers.ModelSerializer):
         model = Join
         fields = ['lead_name', 'telephone']
 
-    def validate_lead_name(self, value):
-        lead_name = value
-        pattern = '^[а-яА-ЯёЁ ]+$'
-        if re.search(pattern, lead_name):
-            return lead_name
-        else:
+
+    def validate(self, data):
+        lead_name = data['lead_name']
+        telephone = data['telephone']
+        pattern_name = '^[а-яА-ЯёЁ ]+$'
+        pattern_phone = '((8|\+?375)[\- ]?)?(\(?\d{2,3}\)?[\- ]?)?[\d\- ]{7,10}'
+        if not re.search(pattern_name, lead_name):
             raise serializers.ValidationError("Введите правильное имя")
-
-
-    def validate_telephone(self, value):
-        telephone = value
-        pattern = '((8|\+?375)[\- ]?)?(\(?\d{2,3}\)?[\- ]?)?[\d\- ]{7,10}'
-        if re.search(pattern, telephone):
-            return telephone
-        else:
+        elif not re.search(pattern_phone, telephone):
             raise serializers.ValidationError("Введите правильный телефон")
+        else:
+            return data
