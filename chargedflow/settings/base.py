3a0922a09a91866bfa30df5e82afad739ebde600
Evgenii <unklerufus@gmail.com>
2018-01-08 00:08:09 +0300

    Update base.py

--- chargedflow/settings/base.py
+++ chargedflow/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g+uua5w@g)xy6&3ahswe)wn0!)1jqg7wg=+o7zc#u@qfd#)bun'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
